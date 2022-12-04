4b56b5d345cb75a35573fb20191be02c08d005ca
Andrea Martinez <andrea.maort@gmail.com>
2022-12-04 11:39:26 +0100

    added variable result as a state which could be set by the buttons

--- src/components/PinPad/PinPad.tsx
+++ src/components/PinPad/PinPad.tsx
@@ -1,21 +1,31 @@
+import { useState } from "react";
 import Button from "../Button/Button";
 import Display from "../Display/Display";
 import "./pinpad.css";
 
 const pinpadNumbers = [7, 8, 9, 4, 5, 6, 3, 2, 1, 0];
+const initialState: string[] = [];
 
 const PinPad = (): JSX.Element => {
+  const [result, setResult] = useState(initialState);
+
+  const handleChange = (digit: number) => {
+    if (result.length < 4) {
+      setResult([...result, digit.toString()]);
+    }
+  };
+
   const digits = pinpadNumbers.map((number) => {
     return (
       <li key={number}>
-        <Button digit={number} />
+        <Button digit={number} handleChange={() => handleChange(number)} />
       </li>
     );
   });
 
   return (
     <div className="pinpad-container">
-      <Display />
+      <Display result={result} />
       <ul className="pinpad">{digits}</ul>
     </div>
   );
