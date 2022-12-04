3f191346b446513f41fb8ad1741eeb2449a59882
Andrea Martinez <andrea.maort@gmail.com>
2022-12-04 12:11:03 +0100

    added error message

--- src/components/PinPad/PinPad.tsx
+++ src/components/PinPad/PinPad.tsx
@@ -7,6 +7,7 @@ const pinpadNumbers = [7, 8, 9, 4, 5, 6, 3, 2, 1, 0];
 const pin = ["8", "5", "3", "0"];
 const initialState: string[] = [];
 const messageOK = ["OK"];
+const messageERROR = ["ERROR"];
 
 const PinPad = (): JSX.Element => {
   const [result, setResult] = useState(initialState);
@@ -18,8 +19,13 @@ const PinPad = (): JSX.Element => {
   };
 
   useEffect(() => {
-    if (pin.toString() === result.toString()) {
-      setTimeout(() => setResult(messageOK), 1000);
+    if (result.length === 4) {
+      if (pin.toString() === result.toString()) {
+        setTimeout(() => setResult(messageOK), 1000);
+      } else {
+        setTimeout(() => setResult(messageERROR), 1000);
+      }
+
       setTimeout(() => setResult([]), 2000);
     }
   }, [result]);
