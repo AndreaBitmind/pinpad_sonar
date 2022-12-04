87b78bc378a951028c20d58bfc3424d0a070f5e5
Andrea Martinez <andrea.maort@gmail.com>
2022-12-04 11:59:58 +0100

    added pin check and OK message

--- src/components/PinPad/PinPad.tsx
+++ src/components/PinPad/PinPad.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "../Button/Button";
 import Display from "../Display/Display";
 import "./pinpad.css";
 
 const pinpadNumbers = [7, 8, 9, 4, 5, 6, 3, 2, 1, 0];
+const pin = ["8", "5", "3", "0"];
 const initialState: string[] = [];
+const messageOK = ["OK"];
 
 const PinPad = (): JSX.Element => {
   const [result, setResult] = useState(initialState);
@@ -14,6 +16,13 @@ const PinPad = (): JSX.Element => {
       setResult([...result, digit.toString()]);
     }
   };
+
+  useEffect(() => {
+    if (pin.toString() === result.toString()) {
+      setTimeout(() => setResult(messageOK), 1000);
+      setTimeout(() => setResult([]), 2000);
+    }
+  }, [result]);
 
   const digits = pinpadNumbers.map((number) => {
     return (
