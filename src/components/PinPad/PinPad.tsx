1ae3b902542409faec32425577a3d37bb7cabb1a
Andrea Martinez <andrea.maort@gmail.com>
2022-12-04 10:59:43 +0100

    all pinpad digits created and implementated

--- src/components/PinPad/PinPad.tsx
+++ src/components/PinPad/PinPad.tsx
@@ -2,11 +2,21 @@ import Button from "../Button/Button";
 import Display from "../Display/Display";
 import "./pinpad.css";
 
+const pinpadNumbers = [7, 8, 9, 4, 5, 6, 3, 2, 1, 0];
+
 const PinPad = (): JSX.Element => {
+  const digits = pinpadNumbers.map((number) => {
+    return (
+      <li key={number}>
+        <Button digit={number} />
+      </li>
+    );
+  });
+
   return (
     <div className="pinpad-container">
       <Display />
-      <Button />
+      <ul className="pinpad">{digits}</ul>
     </div>
   );
 };
